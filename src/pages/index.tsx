454732ba44438b4e9278c806cf221d0cc1573a60
dangorbey <diggerpro45@gmail.com>
2023-09-20 11:59:39 -0400

    removed dynamic classes cuz tailwind

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,54 @@
 import Head from "next/head";
-import { useState } from 'react';
-import GradientButton from "./components/GradientButton";
-import HighlightedText from "./components/HighlightedText";
-import type { Color } from "../../colorConfig";
-import { colorConfig, gradients } from "../../colorConfig";
+import { useState } from "react";
+
+interface ColorDefinition {
+  highlight: string;
+  base: string;
+  hover: string;
+  active: string;
+}
+
+const myColors: Record<string, ColorDefinition> = {
+  amber: {
+    highlight: "bg-gradient-to-r from-amber-200 from-10% via-amber-100 via-30% to-amber-300 to-90%",
+    base: "bg-amber-200",
+    hover: "hover:bg-amber-300",
+    active: "border-2 border-amber-400",
+  },
+  orange: {
+    highlight: "bg-gradient-to-r from-orange-200 from-10% via-orange-100 via-30% to-orange-300 to-90%",
+    base: "bg-orange-200",
+    hover: "hover:bg-orange-300",
+    active: "border-2 border-orange-400",
+  },
+  lime: {
+    highlight: "bg-gradient-to-r from-lime-200 from-10% via-lime-100 via-30% to-lime-300 to-90%",
+    base: "bg-lime-200",
+    hover: "hover:bg-lime-300",
+    active: "border-2 border-lime-400",
+  },
+  cyan: {
+    highlight: "bg-gradient-to-r from-cyan-200 from-10% via-cyan-100 via-30% to-cyan-300 to-90%",
+    base: "bg-cyan-200",
+    hover: "hover:bg-cyan-300",
+    active: "border-2 border-cyan-400",
+  },
+  purple: {
+    highlight: "bg-gradient-to-r from-purple-200 from-10% via-purple-100 via-30% to-purple-300 to-90%",
+    base: "bg-purple-200",
+    hover: "hover:bg-purple-300",
+    active: "border-2 border-purple-400",
+  },
+  pink: {
+    highlight: "bg-gradient-to-r from-pink-200 from-10% via-pink-100 via-30% to-pink-300 to-90%",
+    base: "bg-pink-200",
+    hover: "hover:bg-pink-300",
+    active: "border-2 border-pink-400",
+  },
+};
 
 export default function Home() {
-
-  const colors: Color[] = Object.keys(colorConfig) as Color[];
-  const [selectedGradient, setSelectedGradient] = useState(gradients.amber);
+  const [selectedColor, setSelectedColor] = useState<string | null>(null);
 
   return (
     <>
@@ -20,24 +60,14 @@ export default function Home() {
       <main className="flex min-h-screen flex-col items-center justify-center">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
           <div className="max-w-md text-stone-900">
-            Somehow we realize that great stories are
-            told in conflict, but we are unwilling to
-            embrace the potential greatness of the story
-            we are actually in. {" "}
-            <HighlightedText selectedGradient={selectedGradient}>
-              We think God is unjust, rather than a master storyteller.
-            </HighlightedText>
-            <br />
-            <br />
-            - Donald Miller
           </div>
+          {selectedColor && <p>You've selected: {selectedColor.charAt(0).toUpperCase() + selectedColor.slice(1)}</p>}
           <div className="flex space-x-4">
-            {colors.map(color => (
-              <GradientButton
-                key={color}
-                color={color}
-                selectedGradient={selectedGradient}
-                setSelectedGradient={setSelectedGradient}
+            {Object.entries(myColors).map(([label, colorDef], index) => (
+              <button
+                key={index}
+                className={`w-8 h-8 rounded-full ${colorDef.base} ${colorDef.hover} ${selectedColor === label ? colorDef.active : ''}`}
+                onClick={() => setSelectedColor(label)}
               />
             ))}
           </div>
