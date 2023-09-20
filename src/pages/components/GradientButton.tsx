000eca6653f8ac32d0b08e5746fc94c3246f4612
dangorbey <diggerpro45@gmail.com>
2023-09-20 01:55:30 -0400

    cleaned up .env

---
+++ src/pages/components/GradientButton.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { gradients, Color } from './colorConfig';
+
+type GradientButtonProps = {
+  color: Color;
+  selectedGradient: string;
+  setSelectedGradient: React.Dispatch<React.SetStateAction<string>>;
+};
+
+const GradientButton: React.FC<GradientButtonProps> = ({ color, selectedGradient, setSelectedGradient }) => {
+
+  const handleClick = () => {
+    setSelectedGradient(gradients[color]);
+  };
+
+  const buttonStyle = `
+    w-8 h-8 
+    rounded-full 
+    transition ease-in-out duration-150 
+    ${selectedGradient === gradients[color] ? `bg-${color}-200 border-2 border-${color}-400` : `bg-${color}-200 hover:bg-${color}-300`}
+  `;
+
+  return (
+    <button onClick={handleClick} className={buttonStyle}></button>
+  );
+};
+
+export default GradientButton;
